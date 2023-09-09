c6d4cf57374bf144516c042b5e6dbbff90fb8843
Makads <98026146+Makads@users.noreply.github.com>
2023-09-09 23:10:27 +0800

    Add files via upload

---
+++ app/openapi/documents/config.php
@@ -0,0 +1,11 @@
+<?php
+/*
+ * @ https://EasyToYou.eu - IonCube v11 Decoder Online
+ * @ PHP 7.2 & 7.3
+ * @ Decoder version: 1.0.6
+ * @ Release: 10/08/2022
+ */
+
+return ["Basice", "Public", "User", "Login", "Cart", "Pay", "Host", "Invoices", "Ticket", "Affiliate", "Message", "Log", "Knowledgebase", "News", "Downloads"];
+
+?>
