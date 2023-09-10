fb9a77022e4fb65867393c301a02507ca0262b20
Makads <98026146+Makads@users.noreply.github.com>
2023-09-10 17:44:49 +0800

    Add files via upload

---
+++ app/server/MaintainExctption.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace app\server;
+
+class MaintainExctption extends \Exception
+{
+    public function __construct($arr)
+    {
+        parent::__construct($arr[1], $arr[0]);
+    }
+}
+
+?>
